1a5c2cb8a83363971c94fec7a3f31793ed743939
Андрей Семенов <asemenov@callinsight.ru>
2020-08-10 10:58:44 +0500

    5. Циклы while и for [#339031]. Fix

--- src/main/kotlin/ru/job4j/base/drawX.kt
+++ src/main/kotlin/ru/job4j/base/drawX.kt
@@ -1,18 +1,18 @@
 package ru.job4j.base
 
 fun draw(size: Int) {
-    if (size > 0 && size % 2 != 0) {
-        for (i in 0 until size) {
-            for (j in 0 until size) {
-                if (j == i || j == (size - 1 - i)) print('X') else print(' ')
-            }
-            println()
-        }
-    } else {
+    if (size < 0 || size %2 == 0) {
         println("You should give positive and odd number")
+        return
+    }
+    for (i in 0 until size) {
+        for (j in 0 until size) {
+            if (j == i || j == (size - 1 - i)) print('X') else print(' ')
+        }
+        println()
     }
 }
 
 fun main() {
-    draw(3)
+    draw(5)
 }

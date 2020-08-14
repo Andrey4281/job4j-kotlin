ee6574c559809142ba08e84b6487022d3518f3b1
Андрей Семенов <asemenov@callinsight.ru>
2020-08-14 19:24:53 +0500

    5. Класс-компаньон. [#339039]

--- src/main/kotlin/ru/job4j/tracker/StartUI.kt
+++ src/main/kotlin/ru/job4j/tracker/StartUI.kt
@@ -1,31 +1,34 @@
 package ru.job4j.tracker
 
-object StartUI {
-    private val tracker = Tracker()
-    private val console = SimpleConsole()
+class StartUI private constructor() {
 
-    fun init() {
-        var input = "";
-        while (input != "3") {
-            console.print("Choose one action (1,2,3):")
-            console.print("1) add item")
-            console.print("2) show all items")
-            console.print("3) exit")
-            input = console.readData()
-            if (input == "1") addItem()
-            if (input == "2") showAllItems()
+    companion object {
+        private val tracker = Tracker()
+        private val console = SimpleConsole()
+
+        fun init() {
+            var input = "";
+            while (input != "3") {
+                console.print("Choose one action (1,2,3):")
+                console.print("1) add item")
+                console.print("2) show all items")
+                console.print("3) exit")
+                input = console.readData()
+                if (input == "1") addItem()
+                if (input == "2") showAllItems()
+            }
         }
-    }
 
-    private fun addItem() {
-        console.print("Enter item's name")
-        val name = console.readData()
-        val item = Item(name = name)
-        tracker.add(item)
-    }
+        private fun addItem() {
+            console.print("Enter item's name")
+            val name = console.readData()
+            val item = Item(name = name)
+            tracker.add(item)
+        }
 
-    private fun showAllItems() {
-        tracker.findAll().forEach { println(it) }
+        private fun showAllItems() {
+            tracker.findAll().forEach { println(it) }
+        }
     }
 }
 

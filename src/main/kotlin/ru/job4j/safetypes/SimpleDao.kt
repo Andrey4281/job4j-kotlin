d0dc02a20df0e96427193a6952da1f70529cf9cb
Андрей Семенов <asemenov@callinsight.ru>
2020-09-03 15:38:22 +0500

    7. lateinit [#339058]

--- src/main/kotlin/ru/job4j/safetypes/SimpleDao.kt
+++ src/main/kotlin/ru/job4j/safetypes/SimpleDao.kt
@@ -10,55 +10,69 @@ class SimpleDao: AutoCloseable {
 
     fun init() {
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kotlin_test"
-        ,"admin", "xEmtHgW2")
-
-        val select = {sql: String ->
-            var count: Int = 0
-            connection.createStatement().use { statement ->
-                statement.executeQuery(sql).use {resultSet ->
-                    while (resultSet.next()) {
-                        count++
+        ,"admin", "xEmtHgW2").apply {
+            val select = { sql: String ->
+                var count: Int = 0
+                createStatement().apply {
+                    use {
+                        executeQuery(sql).apply {
+                            use {
+                                while (next()) {
+                                    count++
+                                }
+                            }
+                        }
                     }
                 }
+                "$count row(s) were selected"
             }
-            "$count row(s) were selected"
-        }
 
-        val update = {sql: String ->
-            val count = connection.createStatement().use { statement ->
-                statement.executeUpdate(sql)
+            val update = {sql: String ->
+                var count = 0
+                createStatement().apply {
+                    use {
+                        count = executeUpdate(sql)
+                    }
+                }
+                "$count row(s) were updated"
             }
-            "$count row(s) were updated"
-        }
 
-        val delete = { sql: String ->
-            val count = connection.createStatement().use { statement ->
-                statement.executeUpdate(sql)
+            val delete = { sql: String ->
+                var count = 0
+                createStatement().apply {
+                    use {
+                        count = executeUpdate(sql)
+                    }
+                }
+                "$count row(s) were deleted"
             }
-            "$count row(s) were deleted"
-        }
 
-        val insert = { sql: String ->
-            val count = connection.createStatement().use { statement ->
-                statement.executeUpdate(sql)
+            val insert = {sql: String ->
+                var count = 0
+                createStatement().apply {
+                    use {
+                        count = executeUpdate(sql)
+                    }
+                }
+                "$count row(s) were inserted"
             }
-            "$count row(s) were inserted"
-        }
 
-        dispatcher = mapOf("select" to select, "update" to update,
-            "delete" to delete, "insert" to insert)
+            dispatcher = mapOf("select" to select, "update" to update,
+                "delete" to delete, "insert" to insert)
+        }
     }
 
     fun exec(sql: String): String {
         var res: String = ""
-        if (sql.contains("select", ignoreCase = true)) {
-            res = dispatcher.get("select")!!.invoke(sql)
-        } else if (sql.contains("update", true)) {
-            res = dispatcher.get("update")!!.invoke(sql)
-        } else if (sql.contains("delete", true)) {
-            res = dispatcher.get("delete")!!.invoke(sql)
-        } else if (sql.contains("insert", true)) {
-            res = dispatcher.get("insert")!!.invoke(sql)
+        dispatcher.apply {
+            sql.apply {
+                when {
+                    contains("select", ignoreCase = true) -> res = get("select")!!.invoke(this)
+                    contains("update", true) -> res = get("update")!!.invoke(this)
+                    contains("delete", true) -> res = get("delete")!!.invoke(this)
+                    contains("insert", true) -> res = get("insert")!!.invoke(this)
+                }
+            }
         }
         return res
     }
@@ -69,9 +83,9 @@ class SimpleDao: AutoCloseable {
 }
 
 fun main() {
-    val dao = SimpleDao()
-    dao.init()
-    println(dao.exec("INSERT INTO entity(name) VALUES('test')"))
-    //println(dao.exec("SELECT * FROM entity"))
-    dao.close()
+    SimpleDao().apply {
+        init()
+        println(exec("INSERT INTO entity(name) VALUES('test')"))
+        close()
+    }
 }

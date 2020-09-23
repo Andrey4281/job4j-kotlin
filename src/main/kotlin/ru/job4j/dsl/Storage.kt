3368a79c91bad940f58f5911ec986e1ef8d88e6a
Андрей Семенов <asemenov@callinsight.ru>
2020-09-23 11:50:51 +0500

    1. KDoc. Документация. [#339068]

--- src/main/kotlin/ru/job4j/dsl/Storage.kt
+++ src/main/kotlin/ru/job4j/dsl/Storage.kt
@@ -3,8 +3,22 @@ package ru.job4j.dsl
 import java.sql.ResultSet
 import java.sql.Statement
 
+/**
+ * This class is used for simplification operations with a database.
+ * It is simply wrapper over jdbc that allow you don't worry about a resource management.
+ * You can simply write your sql operations to the database.
+ * You can add extension functions for execution CRUD operations with your entity to class Storage.
+ * To perform it you should use Storage's methods execUpdate and execQuery.
+ * @author asemenov
+ * @since 18.09.2020
+ * @version 1
+ */
 class Storage(private val dataSource: DSLDataSource) {
 
+    /**
+     * Execution update operation to a database.
+     * @param update sql script of update operation.
+     */
     fun execUpdate(update: String): Long? {
         var res: Long? = null
         execSt {
@@ -22,6 +36,11 @@ class Storage(private val dataSource: DSLDataSource) {
         return res
     }
 
+    /**
+     * Execution select operation to database.
+     * @param query sql script of select operation.
+     * @param handler block of code which is needed for processing ResultSet object.
+     */
     fun <T> execQuery(query: String, handler: ResultSet.() -> T): T? {
         var res: T? = null
         execSt {
@@ -49,13 +68,24 @@ class Storage(private val dataSource: DSLDataSource) {
     }
 }
 
+/**
+ * It is example how you can use Storage's extension functions for saving your entity to database.
+ * @param entity it is your entity that you want to save to a database.
+ */
 fun Storage.create(entity: Entity): Long? =
     execUpdate("INSERT INTO entity(name) VALUES('${entity.name}')")
 
+/**
+ * It is an example how you can use Storage's extension functions for updating your entity in a database.
+ * @param entity it is your entity that you want to update in a database.
+ */
 fun Storage.update(entity: Entity) =
     execUpdate("UPDATE entity SET name='${entity.name}' WHERE id=${entity.id}")
 
-
+/**
+ * It is an example how you can use Storage's extension functions for selecting your entities from a database.
+ * @param id we use selecting by id in this example, but you can use your own kind of selecting.
+ */
 fun Storage.findById(id: Long): Entity? {
     return execQuery("SELECT * FROM entity WHERE id=$id") {
         var result: Entity? = null
@@ -67,10 +97,16 @@ fun Storage.findById(id: Long): Entity? {
     }
 }
 
+/**
+ * It is an example how you can use Storage's extension functions for deleting your entities from a database.
+ * @param id we use deleting by id in this example, but you can use your own kind of deleting.
+ */
 fun Storage.deleteById(id: Long) =
     execUpdate("DELETE FROM entity WHERE id=$id")
 
-
+/**
+ * It is a demonstration of using Storage class.
+ */
 fun main() {
     val dataSource = DSLDataSource
         .driverClassName("com.mysql.cj.jdbc.Driver")
